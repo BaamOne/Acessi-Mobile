3230826469f3af413de012ea0ec7d809e5a5a7d8
FernandoEngler <fernando_engler@estudante.sc.senai.br>
2023-11-28 23:15:13 -0300

    Ajustes home

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,13 +1,35 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Box, Image, Stack, Button, HStack } from "native-base";
 
 const HomeComponent = () => {
   return (
     <>
-      {" "}
-      <Text>HomeComponent!</Text>
+      <Stack
+        bg="#fff"
+        flex={1}
+        justifyContent="center"
+        alignItems="flex-start"
+        paddingLeft={12}
+      >
+        <Text>
+          <h1 style={{ color: "blue" }}>Bem vindo ao</h1>
+        </Text>
+        <Image
+          source={require("../../images/acessiLogo.png")}
+          alt="Acessi+"
+          size="md"
+          width="80%"
+          marginBottom="25px"
+        />
+        <HStack space={4}>
+          <Button colorScheme="success"> Cadastre-se </Button>
+          <Button variant="ghost" colorScheme="success">
+            Saiba mais
+          </Button>
+        </HStack>
+      </Stack>
     </>
   );
 };
-
 export default HomeComponent;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -6,13 +6,6 @@ import HomeComponent from "../../components/Home/HomeComponent";
 import { StyleSheet } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-});
-
 const Tab = createMaterialBottomTabNavigator();
 
 const HomePage = () => {
